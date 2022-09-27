c168c3134a5a3838f18c958b5d825048e753a067
Edwar Erazo <edwarerazo321@gmail.com>
2022-09-26 21:40:18 -0500

    repo api syscal

---
+++ src/test/java/com/syscal/apisyscal/ApiSyscalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syscal.apisyscal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSyscalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
